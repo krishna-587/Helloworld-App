3d8b16d3335aaf929dc941dfdc0bc412070c663f
Krishna gopal singh <krishnagopalsingh587@gmail.com>
2025-03-24 12:09:22 +0530

    [Krishna Gopal Singh] Add. Ensure the userName is valid input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Helloworld-App';
+  title = 'Helloworld';
+
+  imgUrl = "https://www.a2nacademy.com/a2n_assets/img/hire-dev/hire-partners/bridge-labz.webp"
+  url = "https://www.bridgelabz.com/"
+
+  userName : string = "";
+  nameError : string = "";
+
+  ngOnInit() : void{
+    this.title = "Hello from BridgeLabz."
+  }
+
+  onClick($event: any){
+    console.log("Save button is clicked!", $event);
+    window.open(this.url , "_blank");
+  }
+
+  onInput($event : any){
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
+  }
 }
