c5974680adc6296c6a0e13327a58aebd952c4dbb
Krishna gopal singh <krishnagopalsingh587@gmail.com>
2025-03-24 11:28:51 +0530

    [Krishna Gopal Singh] Add. Ability To launch Site By clicking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Helloworld-App';
+  title = 'Helloworld';
+
+  imgUrl = "https://www.a2nacademy.com/a2n_assets/img/hire-dev/hire-partners/bridge-labz.webp"
+  url = "https://www.bridgelabz.com/"
+
+  ngOnInit() : void{
+    this.title = "Hello from BridgeLabz."
+  }
+
+  onClick($event: any){
+    console.log("Save button is clicked!", $event);
+    window.open(this.url , "_blank");
+  }
 }
